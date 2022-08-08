410a641c466210c5ead1403028b302a896be73a9
Hazey <hassaanhmd@gmail.com>
2022-08-08 21:25:57 +0200

    changed title of manage expense page to add expense or edit expense depending on the way the page was navigated to

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,11 +1,23 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import { GlobalStyles } from '../constants/styles';
 
-function ManageExpense() {
-   return <View>
-    <Text>This is the Manage Expense page</Text>
-  </View>
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId; // '!!' is a JS method to convert to boolean
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
+
+  return (
+    <View>
+      <Text>This is the Manage Expense page</Text>
+    </View>
+  );
 }
 
 export default ManageExpense;
