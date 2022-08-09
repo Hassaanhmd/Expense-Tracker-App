c787cd283378852e54f996adc4d54f2cfd06cc83
Hazey <hassaanhmd@gmail.com>
2022-08-09 10:32:35 +0200

    styles added to ManageExpense Screen

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, TextInput, View } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 
-function Input({ label, textInputConfig }) {
+function Input({ label, style, textInputConfig }) {
   
   const inputStyles = [styles.input];
 
@@ -9,7 +9,7 @@ function Input({ label, textInputConfig }) {
     inputStyles.push(styles.inputMultiline);
   }
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
